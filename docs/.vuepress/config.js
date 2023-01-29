5cfdd899a519c3be932d56caf050f54c2e301a3c
所无聊 <2409277719@qq.com>
2023-01-30 00:18:13 +0800

    修改webpack配置 publicPath 替换为 cdn 资源

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,44 +1,48 @@
-const nav = require('./config/nav.js')
+const nav = require("./config/nav.js");
 
 module.exports = {
   title: "Jacky's blog",
   description:
-    '有志者、事竟成，破釜沉舟，百二秦zhi关终属楚; 苦心人、天不负，卧薪尝胆，三千越甲可吞吴。', // 描述,以 <meta> 标签渲染到页面html中
+    "有志者、事竟成，破釜沉舟，百二秦zhi关终属楚; 苦心人、天不负，卧薪尝胆，三千越甲可吞吴。", // 描述,以 <meta> 标签渲染到页面html中
   // base: '/', // '/<github仓库名>/'， 默认'/'
-  base: '/jacky-blog-vuepress/',
+  base: "/jacky-blog-vuepress/",
+  configureWebpack: config => {
+    config.output.publicPath =
+      "https://cdn-pic-wjs.oss-cn-shanghai.aliyuncs.com/";
+  },
   head: [
     // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
-    ['link', { rel: 'icon', href: '/img/披萨.png' }], //favicons，资源放在public文件夹
+    ["link", { rel: "icon", href: "img/pizza.png" }], //favicons，资源放在public文件夹
     [
-      'meta',
+      "meta",
       {
-        name: 'keywords',
+        name: "keywords",
         content:
-          '有志者、事竟成，破釜沉舟，百二秦zhi关终属楚；下联：苦心人、天不负，卧薪尝胆，三千越甲可吞吴。'
-      }
+          "有志者、事竟成，破釜沉舟，百二秦zhi关终属楚；下联：苦心人、天不负，卧薪尝胆，三千越甲可吞吴。",
+      },
     ],
-    ['meta', { name: 'baidu-site-verification', content: '7F55weZDDc' }], // 百度统计的站点拥有者验证
-    ['meta', { name: 'theme-color', content: '#11a8cd' }] // 移动浏览器主题颜色
+    ["meta", { name: "baidu-site-verification", content: "7F55weZDDc" }], // 百度统计的站点拥有者验证
+    ["meta", { name: "theme-color", content: "#11a8cd" }], // 移动浏览器主题颜色
     // ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no'}], // 移动端阻止页面缩放
   ],
   markdown: {
-    lineNumbers: true // 代码行号
+    lineNumbers: true, // 代码行号
   },
 
-  theme: 'vdoing', // 使用依赖包主题
+  theme: "vdoing", // 使用依赖包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   themeConfig: {
     // 主题配置
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
-    logo: '/img/披萨.png', // 导航栏logo
-    repo: '/wangjs-jacky', // 导航栏右侧生成Github链接
+    logo: "img/pizza.png", // 导航栏logo
+    repo: "/wangjs-jacky", // 导航栏右侧生成Github链接
     searchMaxSuggestions: 20, // 搜索结果显示最大数
-    lastUpdated: '上次更新', // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
-    docsDir: 'docs', // 编辑的文件夹
+    lastUpdated: "上次更新", // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
+    docsDir: "docs", // 编辑的文件夹
     editLinks: true, // 启用编辑
-    editLinkText: '编辑',
+    editLinkText: "编辑",
 
     //*** 以下配置是Vdoing主题改动和新增的配置 ***//
     // category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
@@ -47,7 +51,7 @@ module.exports = {
     // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
 
     bodyBgImg: [
-      'https://wjs-tik.oss-cn-shanghai.aliyuncs.com/img/337bde0672199d903ab0a962d9e0ca79.jpg'
+      "https://wjs-tik.oss-cn-shanghai.aliyuncs.com/img/337bde0672199d903ab0a962d9e0ca79.jpg",
     ], // body背景大图，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
     bodyBgImgOpacity: 0.8, // body背景图透明度，选值 0 ~ 1.0, 默认0.5
 
@@ -67,45 +71,45 @@ module.exports = {
     // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
     // pageButton: false, // 是否显示快捷翻页按钮，默认true
 
-    sidebar: 'structuring', // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+    sidebar: "structuring", // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
 
     author: {
       // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-      name: 'wangjiasheng', // 必需
-      link: 'https://github.com/wangjs-jacky' // 可选的
+      name: "wangjiasheng", // 必需
+      link: "https://github.com/wangjs-jacky", // 可选的
     },
     blogger: {
       // 博主信息，显示在首页侧边栏
-      avatar: './img/古风头像.jpg',
-      name: 'Jacky Wang',
-      slogan: '行到水穷处，坐看云起时'
+      avatar: "./img/古风头像.jpg",
+      name: "Jacky Wang",
+      slogan: "行到水穷处，坐看云起时",
     },
     social: {
       // 社交图标，显示于博主信息栏和页脚栏
       // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
       icons: [
         {
-          iconClass: 'icon-youjian',
-          title: '发邮件',
-          link: 'mailto:2409277719@qq.com'
+          iconClass: "icon-youjian",
+          title: "发邮件",
+          link: "mailto:2409277719@qq.com",
         },
         {
-          iconClass: 'icon-github',
-          title: 'GitHub',
-          link: 'https://github.com/wangjs-jacky'
+          iconClass: "icon-github",
+          title: "GitHub",
+          link: "https://github.com/wangjs-jacky",
         },
         {
-          iconClass: 'icon-erji',
-          title: '听音乐',
-          link: 'https://music.163.com/#/playlist?id=642386905'
-        }
-      ]
+          iconClass: "icon-erji",
+          title: "听音乐",
+          link: "https://music.163.com/#/playlist?id=642386905",
+        },
+      ],
     },
     footer: {
       // 页脚信息
-      createYear: 2020 // 博客创建年份
+      createYear: 2020, // 博客创建年份
       // copyrightInfo: 'Evan Xu | <a href="https://github.com/xugaoyi/vuepress-theme-vdoing/blob/master/LICENSE" target="_blank">MIT License</a>', // 博客版权信息，支持a标签
-    }
+    },
   },
   plugins: [
     // 插件
@@ -114,46 +118,54 @@ module.exports = {
     //   excludeClassName: 'theme-vdoing-content' // 要排除元素的class, 默认空''
     // }],
 
-    ['thirdparty-search', { // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
-      thirdparty: [ // 可选，默认 []
-        // {
-        //     title: '在MDN中搜索',
-        //     frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
-        //     behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
-        // },
-        // {
-        //     title: '在Runoob中搜索',
-        //     frontUrl: 'https://www.runoob.com/?s=',
-        // },
-        // {
-        //     title: '在Vue API中搜索',
-        //     frontUrl: 'https://cn.vuejs.org/v2/api/#',
-        // },
-        // {
-        //     title: '在Bing中搜索',
-        //     frontUrl: 'https://cn.bing.com/search?q='
-        // },
-        // {
-        //     title: '通过百度搜索本站的',
-        //     frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20'
-        // }
-      ]
-    }],
+    [
+      "thirdparty-search",
+      {
+        // 可以添加第三方搜索链接的搜索框（原官方搜索框的参数仍可用）
+        thirdparty: [
+          // 可选，默认 []
+          // {
+          //     title: '在MDN中搜索',
+          //     frontUrl: 'https://developer.mozilla.org/zh-CN/search?q=', // 搜索链接的前面部分
+          //     behindUrl: '' // 搜索链接的后面部分，可选，默认 ''
+          // },
+          // {
+          //     title: '在Runoob中搜索',
+          //     frontUrl: 'https://www.runoob.com/?s=',
+          // },
+          // {
+          //     title: '在Vue API中搜索',
+          //     frontUrl: 'https://cn.vuejs.org/v2/api/#',
+          // },
+          // {
+          //     title: '在Bing中搜索',
+          //     frontUrl: 'https://cn.bing.com/search?q='
+          // },
+          // {
+          //     title: '通过百度搜索本站的',
+          //     frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20'
+          // }
+        ],
+      },
+    ],
 
     // 'vuepress-plugin-baidu-autopush', // 百度自动推送
 
     [
-      'one-click-copy',
+      "one-click-copy",
       {
         // 代码块复制按钮
-        copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
-        copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+        copySelector: [
+          'div[class*="language-"] pre',
+          'div[class*="aside-code"] aside',
+        ], // String or Array
+        copyMessage: "复制成功", // default is 'Copy successfully and then paste it for use.'
         duration: 1000, // prompt message display time.
-        showInMobile: false // whether to display on the mobile side, default: false.
-      }
+        showInMobile: false, // whether to display on the mobile side, default: false.
+      },
     ],
     [
-      'demo-block',
+      "demo-block",
       {
         // demo演示模块 https://github.com/xiguaxigua/vuepress-plugin-demo-block
         settings: {
@@ -162,24 +174,24 @@ module.exports = {
           // vue: 'https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js', // 在线示例中的vue依赖
           jsfiddle: false, // 是否显示 jsfiddle 链接
           codepen: true, // 是否显示 codepen 链接
-          horizontal: false // 是否展示为横向样式
-        }
-      }
+          horizontal: false, // 是否展示为横向样式
+        },
+      },
     ],
     [
-      'vuepress-plugin-zooming', // 放大图片
+      "vuepress-plugin-zooming", // 放大图片
       {
-        selector: '.theme-vdoing-content img:not(.no-zoom)', // 排除class是no-zoom的图片
+        selector: ".theme-vdoing-content img:not(.no-zoom)", // 排除class是no-zoom的图片
         options: {
-          bgColor: 'rgba(0,0,0,0.6)'
-        }
-      }
+          bgColor: "rgba(0,0,0,0.6)",
+        },
+      },
     ],
     [
-      'vuepress-plugin-baidu-tongji', // 百度统计
+      "vuepress-plugin-baidu-tongji", // 百度统计
       {
-        hm: '503f098e7e5b3a5b5d8c5fc2938af002'
-      }
+        hm: "503f098e7e5b3a5b5d8c5fc2938af002",
+      },
     ],
     // [
     //   'vuepress-plugin-comment', // 评论
@@ -211,16 +223,16 @@ module.exports = {
     //   }
     // ],
     [
-      '@vuepress/last-updated', // "上次更新"时间格式
+      "@vuepress/last-updated", // "上次更新"时间格式
       {
         transformer: (timestamp, lang) => {
-          const moment = require('moment') // https://momentjs.com/
-          return moment(timestamp).format('YYYY/MM/DD, H:MM:SS')
-        }
-      }
+          const moment = require("moment"); // https://momentjs.com/
+          return moment(timestamp).format("YYYY/MM/DD, H:MM:SS");
+        },
+      },
     ],
-    ['latex']
-  ]
+    ["latex"],
+  ],
   // configureWebpack: {
   //   //webpack别名 如![Image from alias](~@alias/image.png)
   //   resolve: {
@@ -229,4 +241,4 @@ module.exports = {
   //     }
   //   }
   // }
-}
+};
