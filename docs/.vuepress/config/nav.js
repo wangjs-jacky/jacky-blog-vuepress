842b6d2884938fa4ccdfa179efb3ba3cf7922aac
所无聊 <2409277719@qq.com>
2023-02-05 14:54:00 +0800

    重新梳理博客网站布局

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -37,7 +37,7 @@ module.exports = [
       },
       {
         text: "组件库封装",
-        items: [{ text: "组件库", link: "/pages/b62cdd/" }]
+        items: [{ text: "组件库", link: "/pages/b62cdd/" }],
       },
       {
         text: "前端部署系列",
@@ -50,7 +50,10 @@ module.exports = [
     link: "/cases/",
     items: [
       { text: "项目展示", link: "/pages/ec6dd3/" },
-      { text: "案例中心", link: "https://wangjs-jacky.github.io/jacky-workspace-html/" },
+      {
+        text: "案例中心",
+        link: "https://wangjs-jacky.github.io/jacky-workspace-html/",
+      },
       { text: "First Project", link: "/pages/4b197e/" },
     ],
   },
@@ -69,64 +72,102 @@ module.exports = [
   {
     text: "编码专题",
     link: "/code/",
-    items: [
-      { text: "Promise专题", link: "/pages/2dd33c/" }
-    ]
+    items: [{ text: "Promise专题", link: "/pages/2dd33c/" }],
   },
   {
     text: "CheetSheet",
-    link: "/cheetSheet/",
     items: [
-      { text: "docker教程", link: "https://www.mubucm.com/doc/jZ6j8HMYWY" },
-      { text: "git-常用指令", link: "https://www.mubucm.com/doc/zCpTjqTTqY" },
-      { text: "git-教程", link: "/pages/5857b5/" },
-      { text: "yarn 指令", link: "https://www.mubucm.com/doc/OgdY8V6GQk" },
-      { text: "lerna 教程", link: "https://www.mubucm.com/doc/jJ4S1L2oaY" },
-      { text: "Nginx 指令", link: "https://www.mubucm.com/doc/7MFCsK2QGY" },
-      { text: "nvm 指令", link: "https://www.mubucm.com/doc/OoflbXC2iY" },
-      { text: "在线API文档", link: "https://devdocs.io/" },
-      { text: "正则手册", link: "https://www.mubucm.com/doc/ivGX4hHh0c" },
-      { text: "nginx配置", link: "https://www.mubucm.com/doc/v9Ricxgh8c" },
-      { text: "fs-extra", link: "https://www.mubucm.com/doc/3XQLr3N-Mc" },
-      { text: "VSCode 调试-launch.json", link: "https://www.mubucm.com/doc/CI-7wLAOzI" },
-      { text: "Linter 上手完全指南", link: "https://www.mubucm.com/doc/zdoA0VGGkc" }
-      /* { text: "npm-cli", link: "https://docs.npmjs.com/cli/v6/commands" },
       {
-        text: "npm-package.json",
-        link:
-          "https://docs.npmjs.com/cli/v6/configuring-npm/package-json#directories",
+        text: "配置文件",
+        items: [
+          {
+            text: "TSCONFIG-配置",
+            link: "https://www.mubucm.com/doc/enPp7O5h2s",
+          },
+          { text: "NGINX-配置", link: "https://www.mubucm.com/doc/v9Ricxgh8c" },
+          {
+            text: "正则规则查询手册",
+            link: "https://www.mubucm.com/doc/ivGX4hHh0c",
+          },
+          {
+            text: "Lint 配置",
+            link: "https://www.mubucm.com/doc/zdoA0VGGkc",
+          },
+        ],
       },
       {
-        text: "npm-shorthands",
-        link:
-          "https://docs.npmjs.com/cli/v8/using-npm/config#shorthands-and-other-cli-niceties",
+        text: "教程",
+        items: [
+          { text: "GIT-教程", link: "/pages/5857b5/" },
+          {
+            text: "DOCKER-教程",
+            link: "https://www.mubucm.com/doc/jZ6j8HMYWY",
+          },
+          { text: "LERNA-教程", link: "https://www.mubucm.com/doc/jJ4S1L2oaY" },
+          {
+            text: "GIT-常用操作整理",
+            link: "https://www.mubucm.com/doc/zCpTjqTTqY",
+          },
+        ],
       },
-      
-      { text: "json", link: "/pages/ddaeb3/" },
-      { text: "nrm", link: "/pages/6a6b62/" },
-      { text: "Array", link: "/pages/1f18e1/" },
-       */
+      {
+        text: "VSCode",
+        items: [
+          {
+            text: "LAUNCH.JSON",
+            link: "https://www.mubucm.com/doc/CI-7wLAOzI",
+          },
+        ],
+      },
+      {
+        text: "指令",
+        items: [
+          { text: "NPM 指令", link: "/pages/6a6b62/" },
+          { text: "NVM 指令", link: "https://www.mubucm.com/doc/OoflbXC2iY" },
+          { text: "Nginx 指令", link: "https://www.mubucm.com/doc/7MFCsK2QGY" },
+          { text: "YARN 指令", link: "https://www.mubucm.com/doc/OgdY8V6GQk" },
+          { text: "PNPM 指令", link: "https://www.mubucm.com/doc/32BvEWOnss" },
+        ],
+      },
+      {
+        text: "库",
+        items: [
+          {
+            text: "FS-EXTRA 库",
+            link: "https://www.mubucm.com/doc/3XQLr3N-Mc",
+          },
+          {
+            text: "NODE 库-PATH",
+            link: "https://www.mubucm.com/doc/4KYSfMIQCY",
+          },
+        ],
+      },
     ],
   },
   {
     text: "资料收集",
-    link: "/notes/",
     items: [
       {
-        text: "前端",
+        text: "永远的神",
         items: [
           {
-            text: "unocss",
-            link: "https://uno.antfu.me/"
+            text: "魔法师卡颂-自顶向下学 React 源码",
+            link: "https://ke.segmentfault.com/mine",
+          },
+          { text: "全栈潇晨", link: "https://xiaochen1024.com/" },
+          {
+            text: "博客-程序员山月-Daily",
+            link: "https://github.com/shfshanyue/Daily-Question",
           },
           {
-            text: "图解 React 原理",
-            link: "https://7kms.github.io/react-illustration-series/"
+            text: "淘系前端：冴羽 ",
+            link: "https://github.com/mqyqingfeng/Blog",
           },
-          {
-            text: "promise",
-            link: "https://juejin.cn/post/6999804617320038408"
-          },
+        ],
+      },
+      {
+        text: "系列文章",
+        items: [
           {
             text: "《图解HTTP》",
             link: "https://ppsteven.github.io/pages/c9e990/",
@@ -136,66 +177,18 @@ module.exports = [
             text: "《现代JavaScript教程》",
             link: "https://zh.javascript.info/",
           },
-          { text: "《深入浅出Webpack》", link: "https://webpack.wuhaolin.cn/" },
-          { text: "小茗同学", link: "http://blog.haoji.me/" },
-          { text: "阮一峰-网道", link: "https://wangdoc.com/" },
           {
-            text: "博客-程序员山月-Daily",
-            link: "https://github.com/shfshanyue/Daily-Question",
+            text: "《深入浅出Webpack》",
+            link: "https://webpack.wuhaolin.cn/",
           },
-          { text: "博客-全栈潇晨", link: "https://xiaochen1024.com/" },
-          { text: "codePen", link: "https://codepen.io/" },
           {
-            text: "DigitalOcean",
-            link: "https://www.digitalocean.com/community",
+            text: "VSCode 插件系列：小茗同学",
+            link: "http://blog.haoji.me/",
           },
-          { text: "underscore源码", link: "https://underscorejs.net/" },
           {
-            text: "在线分析项目中的Node依赖",
-            link: "https://npm.devtool.tech/",
-          },
-          { text: "小浩算法", link: "https://www.geekxh.com" },
-          {
-            text: "labuladong的算法小抄",
-            link: "https://labuladong.github.io/",
-          },
-          { text: "力扣solution", link: "http://leetcode-solution.cn/" },
-          { text: "HackerRank", link: "https://www.hackerrank.com/" },
-          { text: "代码随想录", link: "https://programmercarl.com/" },
-          {
-            text: "study with BGM",
+            text: "JEST 教程",
             link:
-              "https://www.notion.so/Study-with-BGM-514787e9455a4e688100cb35fd0e6554",
-          },
-          { text: "印记中文", link: "https://docschina.org/" },
-          { text: "tool.lu", link: "https://tool.lu/" },
-          {
-            text: "css easeing 库",
-            link: "https://xuanfengge.com/easeing/ceaser/",
-          },
-          { text: "rough.js", link: "https://xuanfengge.com/rough-js.html" },
-          { text: "npm trend", link: "https://www.npmtrends.com/" },
-          { text: "find npm ", link: "https://snyk.io/advisor/" },
-          { text: "css网站收集", link: "/pages/81a79d/" },
-          {
-            text: "优秀博客1",
-            link: "https://juejin.cn/user/254742429175352/posts",
-          },
-          {
-            text: "优秀博客2-umi",
-            link: "https://www.zhihu.com/people/song-meng-xiang-95/posts",
-          },
-          {
-            text: "蜡笔小伟",
-            link: "https://www.jianshu.com/u/491bd4155f96",
-          },
-          {
-            text: "自顶向下学 React 源码",
-            link: "https://ke.segmentfault.com/mine",
-          },
-          {
-            text: "淘系前端：冴羽-underscore系列|ts系列(中国最好)",
-            link: "https://github.com/mqyqingfeng/Blog",
+              "http://github.yanhaixiang.com/jest-tutorial/basic/getting-started",
           },
           {
             text: "前端精读周刊:各种精读系列",
@@ -206,84 +199,217 @@ module.exports = [
             link: "https://juejin.cn/post/6944863057000529933",
           },
           {
-            text: "nginx【B站视频】",
-            link: "https://www.bilibili.com/video/BV1LY411M74k"
+            text: "图解 REACT 原理",
+            link: "https://7kms.github.io/react-illustration-series/",
+          },
+        ],
+      },
+      {
+        text: "实用网站",
+        items: [
+          {
+            text: "MDN",
+            link: "https://developer.mozilla.org/zh-CN/",
           },
           {
-            text: "jest测试手册",
-            link: "http://github.yanhaixiang.com/jest-tutorial/basic/getting-started"
+            text: "FRONT-END TREND",
+            link: "https://risingstars.js.org/2021/zh",
+          },
+          { text: "NPM TREND", link: "https://www.npmtrends.com/" },
+          {
+            text: "在线分析 Node 依赖",
+            link: "https://npm.devtool.tech/",
+          },
+          { text: "FIND NPM ", link: "https://snyk.io/advisor/" },
+          { text: "CODE PEN", link: "https://codepen.io/" },
+          { text: "印记中文", link: "https://docschina.org/" },
+          { text: "TOOL.LU", link: "https://tool.lu/" },
+          { text: "阮一峰-网道", link: "https://wangdoc.com/" },
+          {
+            text: "DIGITAL OCEAN",
+            link: "https://www.digitalocean.com/community",
+          },
+          { text: "DEVDOCS.IO", link: "https://devdocs.io/" },
+          {
+            text: "JOI",
+            link: "https://joi.dev/api/?v=17.6.1",
+          },
+        ],
+      },
+      {
+        text: "算法",
+        items: [
+          { text: "小浩算法", link: "https://www.geekxh.com" },
+          {
+            text: "LABULADONG 的算法小抄",
+            link: "https://labuladong.github.io/",
+          },
+          { text: "力扣 SOLUTION", link: "http://leetcode-solution.cn/" },
+          { text: "HACKER RANK", link: "https://www.hackerrank.com/" },
+          { text: "代码随想录", link: "https://programmercarl.com/" },
+        ],
+      },
+      {
+        text: "博客系列",
+        items: [
+          {
+            text: "美团大佬",
+            link: "https://github.com/atqq/sugar-blog",
+          },
+
+          {
+            text: "蜡笔小伟",
+            link: "https://www.jianshu.com/u/491bd4155f96",
+          },
+          {
+            text: "优秀博客1",
+            link: "https://juejin.cn/user/254742429175352/posts",
+          },
+          {
+            text: "优秀博客2-umi",
+            link: "https://www.zhihu.com/people/song-meng-xiang-95/posts",
           },
           {
             text: "优质博客",
-            link: "https://blog.cjw.design/blog/algorithm/sort"
+            link: "https://blog.cjw.design/blog/algorithm/sort",
           },
-          {
-            text: "joi官网",
-            link: "https://joi.dev/api/?v=17.6.1"
-          },
-          {
-            text: "前端趋势",
-            link: "https://risingstars.js.org/2021/zh"
-          },
-          {
-            text: "美团大佬",
-            link: "https://github.com/atqq/sugar-blog"
-          }
         ],
       },
       {
-        text: "机器学习",
+        text: "CSS",
         items: [
-          { text: "《数字信号处理》", link: "/pages/d94079/" },
-          { text: "《scikit-learn》", link: "/pages/55d218/" },
+          {
+            text: "CSS-EASING 库",
+            link: "https://xuanfengge.com/easeing/ceaser/",
+          },
+          {
+            text: "ROUGH.JS",
+            link: "https://xuanfengge.com/rough-js.html",
+          },
+          { text: "CSS 网站收集", link: "/pages/81a79d/" },
+          {
+            text: "UNOCSS",
+            link: "https://uno.antfu.me/",
+          },
         ],
       },
       {
-        text: "Js基础",
-        link: "/pages/60b3a2/",
+        text: "前端",
+        items: [
+          {
+            text: "PROMISE",
+            link: "https://juejin.cn/post/6999804617320038408",
+          },
+
+          { text: "UNDERSCORE.JS", link: "https://underscorejs.net/" },
+
+          {
+            text: "study with BGM",
+            link:
+              "https://www.notion.so/Study-with-BGM-514787e9455a4e688100cb35fd0e6554",
+          },
+          {
+            text: "nginx【B站视频】",
+            link: "https://www.bilibili.com/video/BV1LY411M74k",
+          },
+
+          {
+            text: "机器学习",
+            items: [
+              { text: "《数字信号处理》", link: "/pages/d94079/" },
+              { text: "《scikit-learn》", link: "/pages/55d218/" },
+            ],
+          },
+          {
+            text: "Js基础",
+            link: "/pages/60b3a2/",
+          },
+        ],
       },
-      // {
-      //   text: '闲书',
-      //   items: [
-      //     {
-      //       text: '《明朝那些事儿》',
-      //       link: 'https://www.notion.so/3f15569850554ec486eea3fb24f9bba3'
-      //     }
-      //   ]
-      // }
     ],
   },
+
   {
-    text: "收藏",
-    link: "/pages/5f8056/",
-    items: [{ text: "网站", link: "/pages/5f8056/" }],
+    text: "掘金",
+    items: [
+      {
+        text: "掘金已购课程",
+        items: [
+          {
+            text: "前端自动化测试精讲",
+            link:
+              "https://juejin.cn/book/7174044519350927395?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "深入浅出 Vite",
+            link:
+              "https://juejin.cn/book/7050063811973218341?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "现代 Web 布局",
+            link:
+              "https://juejin.cn/book/7161370789680250917?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "前端算法与数据结构",
+            link:
+              "https://juejin.cn/book/6844733800300150797?scrollMenuIndex=1",
+          },
+          {
+            text: "基于 Vite 的 SSG 框架开发实战",
+            link:
+              "https://juejin.cn/video/7163857336258265102?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "SSR 实战：官网开发指南",
+            link:
+              "https://juejin.cn/book/7137945369635192836?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "WebGL 入门与实践",
+            link:
+              "https://juejin.cn/book/6844733755580481543?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "玩转 CSS 的艺术之美",
+            link:
+              "https://juejin.cn/book/6850413616484040711?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "前端调试通关秘籍",
+            link:
+              "https://juejin.cn/book/7070324244772716556?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "React 进阶实践指南",
+            link:
+              "https://juejin.cn/book/6945998773818490884?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "TypeScript 全面进阶指南",
+            link:
+              "https://juejin.cn/book/7086408430491172901?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "前端缓存技术与方案解析",
+            link:
+              "https://juejin.cn/book/6994678547826606095?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "npm scripts 前端工作流",
+            link:
+              "https://juejin.cn/book/6844723718749421582?enter_from=course_center&utm_source=course_center",
+          },
+          {
+            text: "Webpack5 核心原理与应用实践",
+            link:
+              "https://juejin.cn/book/7115598540721618944?enter_from=course_center&utm_source=course_center",
+          },
+        ],
+      },
+    ],
+    /* items: [{ text: "网站", link: "/pages/5f8056/" }], */
   },
-  // {
-  //   text: 'MORE',
-  //   link: '/more/',
-  //   items: [
-  //     {
-  //       text: '在线编辑',
-  //       items: [
-  //         {
-  //           text: '待发布内容整理',
-  //           link:
-  //             'https://www.notion.so/905ca970d4ca4f67b27b6491cb33fcff?v=99428ee54e6743c488071d9fb0caa91b'
-  //         }
-  //       ]
-  //     },
-  //     {
-  //       text: '更多',
-  //       items: [
-  //         { text: '声纹识别', link: '/pages/2f2989/' },
-  //         { text: '临时笔记', link: '/pages/f29711/' },
-  //         { text: '面试', link: '/' },
-  //         { text: '心情杂货', link: '/' },
-  //         { text: '友情链接', link: '/friends/' }
-  //       ]
-  //     }
-  //   ]
-  // },
   {
     text: "索引",
     link: "/archives/",
